651fedaebbcd130d916a64d808a448f4d8852f68
biselli-mar <ma741bis@htwg-konstanz.de>
2024-03-24 15:09:21 +0100

    Cut down unnecessarydoubling of suit and rank funcs

--- src/main/scala/models/cardComponent/Card.T.scala
+++ src/main/scala/models/cardComponent/Card.T.scala
@@ -4,13 +4,25 @@ package cardComponent
 import enums.{Suit,Rank}
 
 
+/**
+  * Represents a card with a suit and a rank.
+  * 
+  * Depending on variations of the game, a card can have different ranks.
+  * By overriding the methods `isSpecialCard` and `canBePlacedOn`
+  * you can define the behavior of the cards.
+  * 
+  * @param suit the cards suit (Hearts, Diamonds, Clubs, Spades, Joker)
+  * @param rank the cards rank (Two to Ace)
+  **/
 trait Card(val suit: Suit, val rank: Rank):
-    def isRed: Boolean = suit.isRed
-    def isBlack: Boolean = suit.isBlack
-    def isFace: Boolean = rank.isFace
-    def isNumber: Boolean = rank.isNumber
-    def value: Int = rank.value
-    def isJoker: Boolean = suit.isJoker
-
-    def canBePlacedOn(other: Card): Boolean
+    /**
+      * Special cards trigger unique events in the game.
+      * @return true if the card is a special card, false otherwise
+      */
     def isSpecialCard: Boolean
+    /**
+      * Determines if the card can be placed on another card.
+      * @param other the card to be placed on
+      * @return true if the card can be placed on the other card, false otherwise
+      */
+    def canBePlacedOn(other: Card): Boolean
