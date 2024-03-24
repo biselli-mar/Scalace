852d9107798ee8eb9f8c72a35b44cdd3b99e0d2b
biselli-mar <ma741bis@htwg-konstanz.de>
2024-03-24 14:48:37 +0100

    Extend suit to incorporate Jokers

--- src/main/scala/models/cardComponent/enums/Suit.scala
+++ src/main/scala/models/cardComponent/enums/Suit.scala
@@ -3,17 +3,23 @@ package cardComponent
 package enums
 
 enum CardColor:
-  case Red, Black
+  case Red, Black, Blank
 
 enum Suit(val color: CardColor):
   case Hearts extends Suit(CardColor.Red)
   case Diamonds extends Suit(CardColor.Red)
   case Clubs extends Suit(CardColor.Black)
   case Spades extends Suit(CardColor.Black)
+  case Joker extends Suit(CardColor.Blank)
+
   def isRed: Boolean = this.color == CardColor.Red
   def isBlack: Boolean = this.color == CardColor.Black
+  def isBlank: Boolean = this.color == CardColor.Blank
+  def isJoker: Boolean = this == Joker
+
   def toLetter: String = this match
     case Hearts   => "♥"
     case Diamonds => "♦"
     case Clubs    => "♣"
     case Spades   => "♠"
+    case Joker    => "R"
--- src/test/scala/models/cardComponent/enums/SuitSpec.scala
+++ src/test/scala/models/cardComponent/enums/SuitSpec.scala
@@ -11,12 +11,15 @@ class SuitSpec extends AnyWordSpec:
             Suit.Diamonds.color should be(CardColor.Red)
             Suit.Clubs.color should be(CardColor.Black)
             Suit.Spades.color should be(CardColor.Black)
+            // Jokers are a special suit with no color
+            Suit.Joker.color should be(CardColor.Blank)
         }
         "have a string representation" in {
             Suit.Hearts.toString should be("Hearts")
             Suit.Diamonds.toString should be("Diamonds")
             Suit.Clubs.toString should be("Clubs")
             Suit.Spades.toString should be("Spades")
+            Suit.Joker.toString should be("Joker")
         }
         "have a single letter representation" in {
             // These unicode characters are to be used in the console output
@@ -24,18 +27,35 @@ class SuitSpec extends AnyWordSpec:
             Suit.Diamonds.toLetter should be("♦")
             Suit.Clubs.toLetter should be("♣")
             Suit.Spades.toLetter should be("♠")
+            Suit.Joker.toLetter should be("R")
         }
         "have a method to check if it is red" in {
             Suit.Hearts.isRed should be(true)
             Suit.Diamonds.isRed should be(true)
             Suit.Clubs.isRed should be(false)
             Suit.Spades.isRed should be(false)
+            Suit.Joker.isRed should be(false)
         }
         "have a method to check if it is black" in {
             Suit.Hearts.isBlack should be(false)
             Suit.Diamonds.isBlack should be(false)
             Suit.Clubs.isBlack should be(true)
             Suit.Spades.isBlack should be(true)
+            Suit.Joker.isBlack should be(false)
+        }
+        "have a method to check if it has no color" in {
+            Suit.Hearts.isBlank should be(false)
+            Suit.Diamonds.isBlank should be(false)
+            Suit.Clubs.isBlank should be(false)
+            Suit.Spades.isBlank should be(false)
+            Suit.Joker.isBlank should be(true)
+        }
+        "have a method to check if it is a joker" in {
+            Suit.Hearts.isJoker should be(false)
+            Suit.Diamonds.isJoker should be(false)
+            Suit.Clubs.isJoker should be(false)
+            Suit.Spades.isJoker should be(false)
+            Suit.Joker.isJoker should be(true)
         }
         "be comparable on equality" in {
             Suit.Hearts.equals(Suit.Hearts) should be(Suit.Hearts == Suit.Hearts)
@@ -43,20 +63,30 @@ class SuitSpec extends AnyWordSpec:
             Suit.Hearts == Suit.Diamonds should be(false)
             Suit.Hearts == Suit.Clubs should be(false)
             Suit.Hearts == Suit.Spades should be(false)
+            Suit.Hearts == Suit.Joker should be(false)
 
             Suit.Diamonds == Suit.Hearts should be(false)
             Suit.Diamonds == Suit.Diamonds should be(true)
             Suit.Diamonds == Suit.Clubs should be(false)
             Suit.Diamonds == Suit.Spades should be(false)
+            Suit.Diamonds == Suit.Joker should be(false)
 
             Suit.Clubs == Suit.Hearts should be(false)
             Suit.Clubs == Suit.Diamonds should be(false)
             Suit.Clubs == Suit.Clubs should be(true)
             Suit.Clubs == Suit.Spades should be(false)
+            Suit.Clubs == Suit.Joker should be(false)
 
             Suit.Spades == Suit.Hearts should be(false)
             Suit.Spades == Suit.Diamonds should be(false)
             Suit.Spades == Suit.Clubs should be(false)
             Suit.Spades == Suit.Spades should be(true)
+            Suit.Spades == Suit.Joker should be(false)
+
+            Suit.Joker == Suit.Hearts should be(false)
+            Suit.Joker == Suit.Diamonds should be(false)
+            Suit.Joker == Suit.Clubs should be(false)
+            Suit.Joker == Suit.Spades should be(false)
+            Suit.Joker == Suit.Joker should be(true)
         }
     }
