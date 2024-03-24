d1794f53e3a1583e25709ebbeffc6fe19cdffe02
biselli-mar <ma741bis@htwg-konstanz.de>
2024-03-24 14:49:35 +0100

    Default card implementation

---
+++ src/main/scala/models/cardComponent/defaultImpl/Card.scala
@@ -0,0 +1,23 @@
+package models
+package cardComponent
+package defaultImpl
+
+import enums.{Suit,Rank}
+import cardComponent.Card
+
+
+case class CardImpl(override val suit: Suit, override val rank: Rank) extends Card(suit, rank):
+    def canBePlacedOn(other: Card): Boolean = 
+        if (this.isJoker) true
+        else if (other.isJoker && this.rank == Rank.Ace) true
+        else if (other.isJoker) false
+        else if (other.value <= this.value) true
+        else false
+
+    def isSpecialCard: Boolean =
+        this.rank match
+            case Rank.Two | Rank.Ten => true
+            case _ => false
+
+object CardImpl:
+    def Joker: CardImpl = CardImpl(Suit.Joker, Rank.Ace)
